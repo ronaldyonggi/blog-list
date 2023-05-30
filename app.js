bbfc0e32a717312142b37f3f1dcdf0b5406bf6e0
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-05-30 13:39:27 -0400

    4.2 completed

---
+++ app.js
@@ -0,0 +1,21 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => logger.info('connected to DB!'))
+  .catch(error => logger.error('error connecting DB:', error.message))
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,51 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const PORT = process.env.PORT || 3001
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+app.listen(config.PORT, () => {
+  logger.info(`Server is running on port ${config.PORT}`)
 })
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-  .then(() => console.log('connected to DB!'))
-  .catch(error => console.log('error connecting DB:', error.message))
-
-app.use(cors())
-app.use(express.json())
-
-// Get all the blogs
-app.get('/api/blogs', (request, response) => {
-  Blog.find({})
-    .then(blogs => response.json(blogs))
-})
-
-// Create a blog
-app.post('/api/blogs', (request, response) => {
-  const newBlog = new Blog(request.body)
-
-  newBlog.save()
-    .then(result => response.status(201).json(result))
-})
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
