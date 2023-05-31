1044d442bc1a71726a84649ae4b3419aa19efa6f
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-05-31 12:17:06 -0400

    4.6 mostBlogs done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash/fp')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -18,8 +20,22 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce(blogsMostLikesReducer)
 }
 
+const mostBlogs = (blogs) => {
+  const { flow, countBy, toPairs, maxBy, last, zipObject } = _
+
+  const mostBlogs = flow(
+    countBy('author'), // Group blogs by author
+    toPairs, // Convert to array of [author, quantity] pairs
+    maxBy(last), // Retrieve the entry with the most blogs
+    zipObject(['author', 'blogs']) // Convert to an object of {author:..., blogs: ...}
+  )
+
+  return mostBlogs(blogs)
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
