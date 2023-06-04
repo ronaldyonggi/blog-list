e1f74f322af7345f5e1e78736b740ee4ba094020
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-06-04 18:42:08 -0400

    removed comments

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -15,17 +15,12 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-// Write test for HTTP GET to /api/blogs
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 }, 100000)
-
-// Veify that blog list application returns correct amount of blogs in JSON format
-
-// Once completed, refactor route handler to use async/await
 
 test('all blogs are returned and have correct amount', async () => {
   const response = await api.get('/api/blogs')
