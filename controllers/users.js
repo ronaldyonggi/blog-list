1c58ff85a83e2dc28cb8c1357656d601f26ff66d
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-06-12 16:23:55 -0400

    validate password to be at least 3 characters long

--- controllers/users.js
+++ controllers/users.js
@@ -12,6 +12,11 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  // Validate password is at least 3 chars long
+  if (password.length < 3) {
+    return response.status(400).json({ error: 'password must be at least 3 characters long!' })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
