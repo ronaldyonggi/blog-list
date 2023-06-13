494cc9d039edc7d492b69595d56f4063d2172e68
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-06-13 15:20:22 -0400

    add error handle for cast and jwt error

--- utils/middleware.js
+++ utils/middleware.js
@@ -2,8 +2,14 @@ const logger = require('./logger')
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
-  if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+
+  switch (error.name) {
+    case ('CastError'):
+      return response.status(400).send({ error: 'malformatted id!' })
+    case ('ValidationError'):
+      return response.status(400).send({ error: error.message })
+    case ('JsonWebTokenError'):
+      return response.status(400).send({ error: error.message })
   }
 
   next(error)
