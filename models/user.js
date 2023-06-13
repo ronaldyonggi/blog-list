7825928234f611fd7cb165d68d421febd6f1762b
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-06-13 12:22:04 -0400

    associate the blogs owned by user

--- models/user.js
+++ models/user.js
@@ -14,7 +14,13 @@ const userSchema = new mongoose.Schema({
   },
   passwordHash: {
     type: String,
-  }
+  },
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ]
 })
 
 userSchema.plugin(uniqueValidator)
