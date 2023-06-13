745aa8276e5140c6b967f1ac4a15c6819c7324f5
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-06-13 12:21:03 -0400

    populate the users when blog are fetched

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,7 +4,9 @@ const User = require('../models/user')
 
 // Get all the blogs
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.
+    find({})
+    .populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
