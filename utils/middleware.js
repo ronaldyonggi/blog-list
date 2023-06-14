5b57c0ff40b695c4447758f68f36781ae2ae54a2
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-06-14 17:10:36 -0400

    created userExtractor middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,9 +1,25 @@
+const User = require('../models/user')
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
 
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.startsWith('Bearer ')) {
     request.token = authorization.replace('Bearer ', '')
+  }
+
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  // Only executes if request.token exists
+  if (request.token) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    } else {
+      request.user = await User.findById(decodedToken.id)
+    }
   }
 
   next()
@@ -26,5 +42,6 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   errorHandler,
-  tokenExtractor
+  tokenExtractor,
+  userExtractor
 }
